c2aaba74612fa556b3063b5318ef8188b1cce774
carlos <carloshdelreal@gmail.com>
2019-12-22 10:23:27 -0500

    Setting up some linting rules, setting up prettier

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Calculator(){
-  return (
-    <h1>Todo React Calculator</h1>
-  );
-}
+const Calculator = () => {
+  const hello = 'Hello, This will be a React Calculator';
+  return <h1>{hello}</h1>;
+};
 
-ReactDOM.render(<Calculator />,
-  document.getElementById('root'));
+ReactDOM.render(<Calculator />, document.getElementById('root'));
 
 /*
  * If you want your app to work offline and load faster, you can change
