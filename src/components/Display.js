39737668117f557baa021052387386ca038fde40
carlos <carloshdelreal@gmail.com>
2019-12-23 08:13:18 -0500

    fix linting issues, default prop was never used

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Display = () => {
-  const value = '0';
+const Display = (props) => {
+  const { value } = props;
   return (
     <div className="container display-container">
-      <div className="display">
-        {value}
-      </div>
+      <div className="display">{value}</div>
     </div>
   );
 };
