8609499d61300d78e61079b92721bad9f4c04f95
carlos <carloshdelreal@gmail.com>
2019-12-30 10:52:17 -0500

    simplify the app code

--- src/components/App.js
+++ src/components/App.js
@@ -14,58 +14,8 @@ class App extends React.Component {
     };
   }
 
-  handleNumber(buttonName) {
-    let { total, next } = this.state;
-    const { operation } = this.state;
-    if (operation === null) {
-      if (total === null) {
-        total = buttonName;
-      } else {
-        total += buttonName;
-      }
-    } else if (next === null) {
-      next = buttonName;
-    } else {
-      next += buttonName;
-    }
-    return { next, operation, total };
-  }
-
-  handleOperation(buttonName) {
-    let { total, next, operation } = this.state;
-    const calculator = { next, operation, total };
-    if (operation === null || next === null) {
-      if (buttonName === '+/-') {
-        const { total: calcTotal } = calculate(calculator, buttonName);
-        total = calcTotal;
-      } else {
-        operation = buttonName;
-      }
-    } else {
-      const { total: calcTotal } = calculate(calculator, buttonName);
-      if (buttonName === '=') {
-        next = null;
-        total = calcTotal;
-      } else {
-        next = null;
-        operation = buttonName;
-        total = calcTotal;
-      }
-    }
-    return { next, operation, total };
-  }
-
   handleClick(buttonName) {
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-    const operations = ['+/-', '%', '/', 'X', '-', '+', '='];
-    let { next, operation, total } = this.state;
-    if (buttonName === 'AC' || total === 'Ma Error') {
-      ({ next, operation, total } = { next: null, operation: null, total: null });
-    } else if (numbers.includes(buttonName)) {
-      ({ next, operation, total } = this.handleNumber(buttonName));
-    } else if (operations.includes(buttonName)) {
-      ({ next, operation, total } = this.handleOperation(buttonName));
-    }
+    const { next, operation, total } = calculate(this.state, buttonName);
     this.setState({
       next,
       operation,
